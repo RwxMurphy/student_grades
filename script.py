b7327596ede69d2dccfa493519b51cf56d96b934
amurfie2003 <amurfie2003@gmail.com>
2024-05-10 11:03:00 -0400

    create grade class

--- script.py
+++ script.py
@@ -3,7 +3,16 @@ class Student:
         self.name = name
         self.year = year
 
+class Grade:
+    minimum_passing = 60
+    def __init__(self, score):
+        self.score = score
+    
+
+
+
 # create 3 student instances
 roger = Student("Roger van der Weyden", 10)        
 sandro = Student("Sandro Botticelli", 12)
 pieter = Student("Pieter Bruegel the Elder", 8)
+
