e0fde003d5fcf6518d39a760ad4b35f0e511e6ce
amurfie2003 <amurfie2003@gmail.com>
2024-05-10 10:56:26 -0400

    create three instances of Student class

--- script.py
+++ script.py
@@ -2,4 +2,8 @@ class Student:
     def __init__(self, name, year):
         self.name = name
         self.year = year
-        
+
+# create 3 student instances
+roger = Student("Roger van der Weyden", 10)        
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
